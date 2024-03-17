d6809ad07f77c648a6ac60ca0da94321041cf569
cmkxak <gcm466@naver.com>
2024-03-17 21:34:42 +0900

    refactor : builder 패턴 적용

--- src/main/java/me/project/batch/application/DormantBatchJobConfig.java
+++ src/main/java/me/project/batch/application/DormantBatchJobConfig.java
@@ -1,7 +1,10 @@
 package me.project.batch.application;
 
+import me.project.batch.batchcore.ItemReader;
 import me.project.batch.batchcore.Job;
+import me.project.batch.batchcore.Job.JobBuilder;
 import me.project.batch.batchcore.SimpleTasklet;
+import me.project.batch.domain.Customer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +13,19 @@ public class DormantBatchJobConfig {
 
     @Bean
     public Job dormantBatchJob(
-            SimpleTasklet dormantBatchTasklet,
+            DormantBatchItemReader itemReader,
+            DormantBatchItemProcessor itemProcessor,
+            DormantBatchItemWriter itemWriter,
             DormantJobExecutionListener dormantJobExecutionListener
     ) {
-        return new Job(
-                dormantBatchTasklet, dormantJobExecutionListener
-        );
+
+        return Job.builder()
+                .itemReader(itemReader)
+                .itemProcessor(itemProcessor)
+                .itemWriter(itemWriter)
+                .jobExecutionListener(dormantJobExecutionListener)
+                .build();
+
     }
 
 
